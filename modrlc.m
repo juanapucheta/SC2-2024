4f8f724f2541988b46c66a00cbc6b15f86e52c82
juanapucheta <162345319+juanapucheta@users.noreply.github.com>
2024-03-30 19:53:50 -0300

    Add files via upload

--- modrlc.m
+++ modrlc.m
@@ -1,9 +1,12 @@
 %Funcion modelo
 
 function [X]=modrlc(t_etapa, xant, accion)
-h=1e-6;t_simul=1e-3;R=47;L=1;C=100e-9;
-A=[-R/L, -1/L; 1/C, 0];B=[1/L;0];%
-C=[R 0]; u=accion;
+h=1e-9;t_simul=1e-3;R=47;L=1e-6;C=100e-9;
+
+A=[-R/L, -1/L; 1/C, 0];
+B=[1/L;0];
+C=[R 0];
+u=accion;
 x=xant;
 
 %CALCULA LA EVOLUCION DE LA SEÑAL EN EL TIEMPO
